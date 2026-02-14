07028f19938848f3ec1bad3eb1521460325691ae
Your Name <youremail@example.com>
2026-02-14 09:43:28 +0400

    Remove redirect loop

--- next.config.js
+++ next.config.js
@@ -1,16 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async redirects() {
-    return [
-      // www -> non-www
-      {
-        source: "/:path*",
-        has: [{ type: "host", value: "www.simcolgroup.com" }],
-        destination: "https://simcolgroup.com/:path*",
-        permanent: true,
-      },
-    ];
-  },
-};
-
+const nextConfig = {};
 module.exports = nextConfig;
